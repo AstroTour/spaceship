e06a7ff601bbe6c2a0b3975900833efb9e6890e5
AttilaPfiffer <pifuati@gmail.com>
2024-11-21 10:04:34 +0100

    planets adatfeltoltes

--- database/migrations/2024_10_10_135552_create_planets_table.php
+++ database/migrations/2024_10_10_135552_create_planets_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use app\Models\Planet;
 
 return new class extends Migration
 {
@@ -17,6 +18,54 @@ return new class extends Migration
             $table->text('information');
             $table->timestamps();
         });
+
+        Planet::create([
+           'name' => 'Merkúr',
+            'information' => 'A római szerelem és szépség istennőjéről van elnevezve. Az ókori időkben Vénuszt gyakran két különböző csillagnak tartották, az esti csillagnak és a reggeli csillagnak.'
+        ]);
+
+        Planet::create([
+           'name' => 'Vénusz',
+            'information' => 'A Naphoz legközelebbi bolygó. Gyorsabban kerüli meg a Napot, mint az összes többi bolygó, ezért nevezték el a rómaiak a gyors lábú hírnök istenükről.'
+        ]);
+
+        Planet::create([
+           'name' => 'Föld',
+            'iformation' => 'Föld, otthonunk. Ez az egyetlen bolygó, amelyről ismert, hogy légköre szabad oxigént tartalmaz, folyékony víz óceánjai találhatók a felszínén, és természetesen élet is létezik rajta.'
+        ]);
+
+        Planet::create([
+           'name' => 'Mars',
+            'information' => 'A Naphoz negyedik bolygó és a második legkisebb bolygó a Naprendszerben. A római háború istenéről van elnevezve, amelyet gyakran „Vörös Bolygóként” emlegetnek.'
+        ]);
+
+        Planet::create([
+           'name' => 'Jupiter',
+            'indormation' => 'A Jupiter a legnagyobb bolygó a Naprendszerben. Illően a római mitológia istenek királyáról nevezték el.'
+        ]);
+
+        Planet::create([
+            'name' => 'Szaturnusz',
+            'information' => 'A Szaturnusz a Naphoz hatodik bolygó és a második legnagyobb bolygó a Naprendszerben. A Szaturnusz a római neve Cronusnak, a Titánok urának.'
+        ]);
+
+        Planet::create([
+           'name' => 'Uránusz',
+            'information' => 'Az első bolygó, amelyet a tudósok felfedeztek. A bolygó különleges a drámai dőlésszöge miatt, amelynek következtében tengelye szinte közvetlenül a Nap felé mutat.'
+        ]);
+
+        Planet::create([
+           'name' => 'Neptunusz',
+           'information' => 'A Neptunusz a Naprendszer nyolcadik bolygója, és a legmesszebb eső gázóriás. Jellemzője a jellegzetes kék szín, amelyet a metán jelenléte ad, és erős szelei, amelyek a leggyorsabbak a bolygók között.'
+        ]);
+
+        Planet::create([
+            'name' => 'Pluto',
+            'information' => 'A Plútó, amelyet egykor a Naprendszer kilencedik és legmesszebb eső bolygójának tartottak, ma a legnagyobb ismert törpebolygó a Naprendszerben.'
+        ]);
+
+        
+
     }
 
     /**
