edcf317040c9e91bcd6c084d299ec39959b61445
AlexandraLukacs <lszandika15@gmail.com>
2025-03-19 20:12:50 +0100

    foglalás, módosítás

--- config/session.php
+++ config/session.php
@@ -169,8 +169,8 @@ return [
     | the cookie from being sent to you when it can't be done securely.
     |
     */
-
-    'secure' => env('SESSION_SECURE_COOKIE'),
+    
+    'secure' => env('SESSION_SECURE_COOKIE', false),
 
     /*
     |--------------------------------------------------------------------------
